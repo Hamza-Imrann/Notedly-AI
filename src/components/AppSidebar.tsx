94a7747491cde5228dbe323c75c779eb4fa7bca2
Hamza Imran <hamzaimran.dev@gmail.com>
2025-08-24 19:26:04 +0500

    feat: add sidebar component with notes display

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,48 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+import { Note } from "@/generated/prisma"
+import { getUser } from "@/supabase/server"
+import prisma from "../../prisma/prisma"
+import SidebarGroupContent from "@/components/SidebarGroupContent"
+
+export async function AppSidebar() {
+  const user = await getUser()
+  let notes: Note[] = []
+
+  console.log("User in AppSidebar:", user?.user_metadata.email, user?.user_metadata.name)
+  if (user) {
+    // Fetch notes for the authenticated user
+    await prisma.note.findMany({
+      where: { authorId: user.id },
+      orderBy: { updatedAt: "desc" },
+    })
+      .then((fetchedNotes) => {
+        notes = fetchedNotes
+      })
+      .catch((error) => {
+        console.error("Error fetching notes:", error)
+      })
+  }
+
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-lg font-semibold">
+            {user ? `Notes (${notes.length})` : "Log in to see your notes"}
+          </SidebarGroupLabel>
+          { user && <SidebarGroupContent notes={notes} />}
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
---
+++ src/components/SidebarGroupContent.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { Note } from "@/generated/prisma";
+
+export default function SidebarGroupContent({ notes }: { notes: Note[] }) {
+  return (
+    <div>SidebarGroupContent</div>
+  )
+}
